2da720534887a6394d163db00779780dda72bcca
Luis Gudiel <luisgudiel1711@gmail.com>
2025-02-26 21:13:07 -0600

    ,

--- src/api.js
+++ src/api.js
@@ -2,27 +2,22 @@ import axios from "axios";
 
 const API_URL = "https://cafeteriabackend-prueba.up.railway.app"; // Backend en Railway
 
-export const obtenerAlumnos = async () => {
+export const obtenerAlumno = async (id) => {
     try {
-        const response = await axios.get(`${API_URL}/alumnos`);
+        const response = await axios.get(`${API_URL}/alumnos/${id}`);
         return response.data;
     } catch (error) {
-        console.error("Error obteniendo alumnos:", error);
-        return [];
+        console.error("Error obteniendo alumno:", error);
+        return null;
     }
 };
 
-export const registrarConsumo = async (idAlumno) => {
+export const registrarConsumo = async (data) => {
     try {
-        const fechaHoy = new Date().toISOString().split("T")[0]; // Fecha actual en formato YYYY-MM-DD
-        await axios.post(`${API_URL}/registrar_consumo`, {
-            id_alumno: idAlumno,
-            id_paquete: 1, // Suponiendo que el paquete tiene ID 1
-            fecha: fechaHoy
-        });
-        return true;
+        const response = await axios.post(`${API_URL}/registrar_consumo`, data);
+        return response.data;
     } catch (error) {
         console.error("Error registrando consumo:", error);
-        return false;
+        return null;
     }
 };
